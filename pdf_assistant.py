02db1192278c3030067fa476f762e09bcc51cce2
rahulsingh1397 <162925214+rahulsingh1397@users.noreply.github.com>
2025-01-14 21:02:29 -0500

    Add files via upload

--- pdf_assistant.py
+++ pdf_assistant.py
@@ -1,8 +1,8 @@
 import typer
-from typer import Optional, list
+from typing import Optional, List
 from phi.assistant import Assistant
 from phi.storage.assistant.postgres import PgAssistantStorage
-from phi.knowledge.pdf import PDFKnowledgeBase
+from phi.knowledge.pdf import PDFUrlKnowledgeBase
 from phi.vectordb.pgvector import PgVector2 
 
 import os
@@ -12,8 +12,8 @@ load_dotenv()
 os.environ["GROQ_API_KEY"]=os.getenv("GROQ_API_KEY")
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
-knowledge_base = PDFKnowledgeBase(
-    urls=["https://phi-public.c3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
     vector_db=PgVector2(collection="recipes",db_url=db_url)
 )
 
